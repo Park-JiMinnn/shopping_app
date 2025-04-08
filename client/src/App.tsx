bf1e7eada0c5ae0f8d1df448cfd8b85c67ca5c03
Park-JiMinnn <min.projects0222@gmail.com>
2025-04-09 03:47:40 +0900

    Feat:상품 삭제, 수정 기능 추가 중(4상-42)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,13 +7,45 @@ interface ProductType {
   explanation: string;
   price: number;
 }
+
+interface ProductItemProps {
+  product: ProductType;
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
+  const { id, name, price, explanation } = product;
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  return (
+    <div>
+      <div>{id}</div>
+      <div>{name}</div>
+      <div>{price}</div>
+      <div>{explanation}</div>
+
+      <button type='button' onClick={() =>
+        console.log('삭제하기')}
+      >
+        삭제하기
+      </button>
+
+      <button type='button' onClick={() =>
+        console.log('수정하기기')}
+      >
+        수정정하기
+      </button>
+    </div>
+  );
+};
+
+
 function App() {
 
   const [products, setProducts] = useState<ProductType[]>([
     {
       id: 0,
       name: 'Iphone 13 Max',
-      explanation: '디스플레이는 6.1인치 19.5:9 비율의 2532x1170 해상도를 지원하며 패널 형식은 아몰라방식으 슈퍼어쩌구 디스플레이이다. 인치당 픽셀 수는 몇개이다. 좋은 기능 다 있고 우리 제품 개쩔고', 
+      explanation: '디스플레이는 6.1인치 19.5:9 비율의 2532x1170 해상도를 지원하며 패널 형식은 아몰라방식으 슈퍼어쩌구 디스플레이이다. 인치당 픽셀 수는 몇개이다. 좋은 기능 다 있고 우리 제품 개쩔고',
       price: 1230000,
     },
   ]);
@@ -21,73 +53,61 @@ function App() {
   const [name, setName] = useState('');
   const [explanation, setExplanation] = useState('');
   const [price, setPrice] = useState(0);
+
   const fakeId = useRef(0);
 
   const handleCreate = (newProduct: Omit<ProductType, 'id'>) => {
     fakeId.current += 1;
-    setProducts([
-      ...products,
-      {
-        id: fakeId,
-        name,
-        explanation,
-        price,          
-      },
-    ]);
-  }
-  
-  
-  
+    setProducts([...products, {
+      ...newProduct,
+      id: fakeId.current,
+    }]);
+  };
 
   return (
     <>
-    <form onSubmit={(event) => {
-        event.preventDefault();      
-        fakeId += 1;
-        setProducts([
-          ...products,
-          {
-            id: fakeId,
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleCreate({
             name,
             explanation,
-            price,          
-          },
-        ]);
-      }}
-    >
+            price,
+          });
+        }}
+      >
 
-      <input 
-        value={name}
-        onChange={(event) => setName(event.target.value)}       
-        type='text' 
-        placeholder='상품 이름' 
-      />
+        <input
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          type='text'
+          placeholder='상품 이름'
+        />
 
-      <input 
-        value={explanation}
-        onChange={(event) => setExplanation(event.target.value)}       
-        type='text' 
-        placeholder='상품 설명' 
-      />
-      
-      <input 
-        value={price}
-        onChange={(event) => setPrice(parseInt(event.target.value, 10))}       
-        type='number' 
-        placeholder='상품 가격' 
-      />
+        <input
+          value={explanation}
+          onChange={(event) => setExplanation(event.target.value)}
+          type='text'
+          placeholder='상품 설명'
+        />
 
-      <input type='submit' value='상품 만들기' />
-    </form>
+        <input
+          value={price}
+          onChange={(event) => setPrice(parseInt(event.target.value, 10))}
+          type='number'
+          placeholder='상품 가격'
+        />
 
-    {products.map((product) => (
-      <div>
-        <div>{products[0].id}</div>
-        <div>{products[0].name}</div>
-        <div>{products[0].price}</div>
-        <div>{products[0].explanation}</div>
-      </div>
-    ))}
+        <input type='submit' value='상품 만들기' />
+      </form>
+
+      {products.map(({ id, name, price, explanation }) => (
+        <div key={id}>
+          <div>{name}</div>
+          <div>{price}</div>
+          <div>{explanation}</div>
+        </div>
+      ))}
     </>
   );
 }
